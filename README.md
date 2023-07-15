f52e6ab07b03e03b3542d5357c2226d94e9dcd27
Milad akarie <milad.akarie@gmail.com>
2023-07-15 16:14:33 +0300

    add docs

--- README.md
+++ README.md
@@ -1,39 +1,134 @@
-<!--
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
 
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages).
+<p align="center">                    
+<a href="https://img.shields.io/badge/License-MIT-green"><img src="https://img.shields.io/badge/License-MIT-green" alt="MIT License"></a>                    
+<a href="https://github.com/Milad-Akarie/animate_to/stargazers"><img src="https://img.shields.io/github/stars/Milad-Akarie/animate_to?style=flat&logo=github&colorB=green&label=stars" alt="stars"></a>                    
+<a href="https://pub.dev/packages/animate_to"><img src="https://img.shields.io/pub/v/animate_to.svg?label=pub&color=orange" alt="pub version"></a>                    
+</p>                    
 
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages).
--->
+<p align="center">                  
+<a href="https://www.buymeacoffee.com/miladakarie" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" height="30px" width= "108px"></a>                  
+</p>                  
+---
 
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
-
-## Features
-
-TODO: List what your package can do. Maybe include images, gifs, or videos.
-
-## Getting started
-
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
-
-## Usage
-
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder.
-
+## Basic usage
+1- Create an `AnimateToController`
 ```dart
-const like = 'sample';
+final _animateToController = AnimateToController();
+ ``` 
+2- Wrap the target widget ( the one to animate to) with an `AnimateTo` widget and pass it the controller
+```dart
+AnimateTo(
+    controller: _animateToController,
+    child: const Text('Target'),
+ ),
+```
+3- Wrap the widgets you wish to animate with `AnimateFrom` widget.
+The `AnimateFrom` widget requires a global key which you can create an pass the normal way or have the `AnimateToController` generate it for you from a given tag.
+ ```dart
+ AnimateFrom(
+    key: _animateToController.tag('animate1'), // regularGlobalKey
+    child: const Text('Animatable'),
+  ),
+```
+now trigger the animation by calling
+  ```dart
+ _animateToController.animateTag('animate1');
+  // or by key 
+ _animateToController.animate(regularGlobalKey);
+  ```
+#### complete code
+ ```dart
+Scaffold(
+  body: SizedBox(
+    height: 300,
+    child: Column(
+      children: [
+        AnimateTo(
+          controller: _animateToController,
+          child: const Text('Target'),
+        ),
+        const SizedBox(height: 100),
+        AnimateFrom(
+          key: _animateToController.tag('animate1'),
+          child: const Text('Animatable'),
+        ),
+        const SizedBox(height: 24),
+        ElevatedButton(
+          onPressed: () {
+            _animateToController.animateTag('animate1');
+          },
+          child: const Text('Animate'),
+        ),
+      ],
+    ),
+  ),
+)
 ```
 
-## Additional information
+#### Result
+![](https://github.com/Milad-Akarie/animate_to/blob/main/art/demo_1.gif?raw=true)
 
-TODO: Tell users more about the package: where to find more information, how to
-contribute to the package, how to file issues, what response they can expect
-from the package authors, and more.
+### Animating The target widget (the child of AnimateTo)
+AnimateTo provides a builder property to animate it's child on new arrivals (On Single animation completion)
+ ```dart
+AnimateTo(
+    controller: _animateToController,
+    child: MyCartIcon(),
+    builder: (context, child, animation) {
+      return Transform.translate(
+        offset: Offset(sin(animation.value * 3 * pi) * 3, 0),
+        child: child,
+      );
+    }
+),
+ ```
+![](https://github.com/Milad-Akarie/animate_to/blob/main/art/demo_2.gif?raw=true)
+
+### Transporting data along with the animation
+It's possible to have `AnimateFrom` widget carry data to the target widget by assigning the data you need to transport to the `AnimateFrom.value` property and receive it inside `AnimateTo.onArrival` callback
+ ```dart
+SizedBox(
+    height: 320,
+    width: 240,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        AnimateTo<Color>(
+          controller: _animateToController,
+          child: Circle(size: 100, color: targetColor),
+          onArrival: (color) { // called on animation complete
+            setState(() {
+              targetColor = Color.lerp(targetColor, color, .5)!;
+            });
+          },
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            for (int i = 0; i < 3; i++)
+              AnimateFrom<Color>(
+               /// whatever is passed here is received inside of `AnimateTo.onArrival`
+                value: [Colors.red, Colors.green, Colors.orange][i],
+                key: _animateToController.tag(i),
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(24),
+                  onTap: () => _animateToController.animateTag(i),
+                  child: Circle(
+                    size: 50,
+                    color: [Colors.red, Colors.green, Colors.orange][i].withOpacity(.85),
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ],
+    ),
+  )
+ ```
+![](https://github.com/Milad-Akarie/animate_to/blob/main/art/demo_3.gif?raw=true)
+
+### Support animate_to
+
+You can support animate_to by liking it on Pub and staring it on Github, sharing ideas on how we
+can enhance a certain functionality or by reporting any problems you encounter and of course buying
+a couple coffees will help speed up the development process.
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -36,21 +36,6 @@ class MyHomeScreen extends StatefulWidget {
 class _MyHomeScreenState extends State<MyHomeScreen> {
   final _animateToController = AnimateToController();
 
-  final _circleAnimationSequence = TweenSequence<double>([
-    TweenSequenceItem<double>(
-      tween: Tween(begin: 1, end: 3),
-      weight: 40,
-    ),
-    TweenSequenceItem<double>(
-      tween: Tween(begin: 3, end: .2),
-      weight: 40,
-    ),
-    TweenSequenceItem<double>(
-      tween: Tween(begin: .2, end: 1),
-      weight: 20,
-    ),
-  ]);
-
   static final _scaleTransactionSequence = TweenSequence<double>([
     TweenSequenceItem<double>(
       tween: Tween(begin: 1, end: 3),
@@ -77,7 +62,6 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
       appBar: AppBar(
         title: Text(widget.title),
         actions: [
-          if (false)
             Badge(
               label: Text(
                 "$_itemsCount",
@@ -110,138 +94,51 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
             ),
         ],
       ),
-      body: Column(
-        children: [
-          AnimateTo(
-            controller: _animateToController,
-            child: // cart icon,
-            builder: (context, child, animation) {
-              return Transform.scale(
-                scale: _circleAnimationSequence.animate(animation).value,
-                child: child,
-              );
-            }
-          ),
-          const SizedBox(height: 100),
-          AnimateFrom(
-            key: _animateToController.tag('animate1'),
-            child: const Text('Animatable'),
-          ),
-          const SizedBox(height: 24),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Animate'),
-          ),
-          Expanded(
-            child: Center(
-              child: SizedBox(
-                height: 320,
-                width: 240,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    AnimateTo<Color>(
-                      controller: _animateToController,
-                      child: Circle(size: 100, color: targetColor),
-                      onArrival: (color) {
-                        setState(() {
-                          targetColor = Color.lerp(targetColor, color, .5)!;
-                        });
-                      },
+      body: ListView.builder(
+        itemCount: 20,
+        padding: const EdgeInsets.all(8),
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text("Item $index"),
+              subtitle: Text("Subtitle $index"),
+              leading: AnimateFrom<int>(
+                  value: index,
+                  key: _animateToController.tag(index),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.network(
+                      'https://picsum.photos/100/100?random=$index',
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        for (int i = 0; i < 3; i++)
-                          AnimateFrom<Color>(
-                            value: [Colors.red, Colors.green, Colors.orange][i],
-                            key: _animateToController.tag(i),
-                            child: InkWell(
-                              borderRadius: BorderRadius.circular(24),
-                              onTap: () => _animateToController.animateTag(i),
-                              child: Circle(
-                                size: 50,
-                                color: [Colors.red, Colors.green, Colors.orange][i].withOpacity(.85),
-                              ),
-                            ),
-                          ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          if (false)
-            Expanded(
-              child: ListView.builder(
-                itemCount: 20,
-                padding: const EdgeInsets.all(8),
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: ListTile(
-                      title: Text("Item $index"),
-                      subtitle: Text("Subtitle $index"),
-                      leading: AnimateFrom<int>(
-                          value: index,
-                          key: _animateToController.tag(index),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(8),
-                            child: Image.network(
-                              'https://picsum.photos/100/100?random=$index',
-                            ),
-                          ),
-                          builder: (_, child, animation) {
-                            return Opacity(
-                              opacity: .85,
-                              child: ScaleTransition(
-                                scale: _scaleTransactionSequence.animate(animation),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadiusTween(
-                                    begin: BorderRadius.circular(8),
-                                    end: BorderRadius.circular(50),
-                                  ).evaluate(animation),
-                                  child: child,
-                                ),
-                              ),
-                            );
-                          }),
-                      trailing: IconButton(
-                        icon: const Icon(
-                          Icons.add_shopping_cart_outlined,
+                  ),
+                  builder: (_, child, animation) {
+                    return Opacity(
+                      opacity: .85,
+                      child: ScaleTransition(
+                        scale: _scaleTransactionSequence.animate(animation),
+                        child: ClipRRect(
+                          borderRadius: BorderRadiusTween(
+                            begin: BorderRadius.circular(8),
+                            end: BorderRadius.circular(50),
+                          ).evaluate(animation),
+                          child: child,
                         ),
-                        onPressed: () {
-                          _animateToController.animateTag(index);
-                        },
                       ),
-                    ),
-                  );
+                    );
+                  }),
+              trailing: IconButton(
+                icon: const Icon(
+                  Icons.add_shopping_cart_outlined,
+                ),
+                onPressed: () {
+                  _animateToController.animateTag(index);
                 },
               ),
             ),
-        ],
+          );
+        },
       ),
     );
   }
 }
-
-class Circle extends StatelessWidget {
-  const Circle({super.key, required this.size, required this.color});
-
-  final double size;
-  final Color color;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: size,
-      height: size,
-      decoration: BoxDecoration(
-        color: color,
-        shape: BoxShape.circle,
-      ),
-    );
-  }
-}
-
 
